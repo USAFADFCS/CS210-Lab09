76522cec25ccb3a983e2fc5e1c3670b8b80dd77a
troywein <45960380+troywein@users.noreply.github.com>
2023-08-25 11:13:10 -0600

    removed pre/post conditions

--- cs210sil.h
+++ cs210sil.h
@@ -9,39 +9,29 @@
 /** ----------------------------------------------
  * @brief safely read one integer from stdin
  * @return the integer value read (if successful)
- * pre none
- * post function exits if no integer was read
  */
 int getIntegerSafe();
 
 /** ----------------------------------------------
  * @brief safely read one long from stdin
  * @return the long value read (if successful)
- * pre none
- * post function exits if no long was read
  */
 long getLongSafe();
 
 /** ----------------------------------------------
  * @brief safely read one float from stdin
  * @return the float value read (if successful)
- * pre none
- * post function exits if no float was read
  */
 float getFloatSafe();
 
 /** ----------------------------------------------
  * @brief safely read one double from stdin
  * @return the double value read (if successful)
- * pre none
- * post function exits if no double was read
  */
 double getDoubleSafe();
 
 /** ----------------------------------------------
  * @brief safely read one char from stdin
  * @return the char value read (if successful)
- * pre none
- * post function exits if no char was read
  */
 char getCharSafe();
